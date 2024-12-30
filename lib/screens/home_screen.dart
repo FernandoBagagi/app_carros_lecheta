1a28bf9ca00e846940967bb22847afc839913239
Fernando Bagagi <fernandosmartphone96@gmail.com>
2024-12-30 14:53:34 -0300

    cria as tabs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,22 +8,86 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final Map<String, List<Map<String, String>>> carData = {
+    "Clássicos": [
+      {
+        "title": "Chevrolet Bel Air",
+        "image": "https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg"
+      },
+      {
+        "title": "Ford Mustang 1967",
+        "image": "https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg"
+      },
+    ],
+    "Luxo": [
+      {
+        "title": "Rolls Royce Phantom",
+        "image": "https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg"
+      },
+      {
+        "title": "Bentley Continental",
+        "image": "https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg"
+      },
+    ],
+    "Esportivos": [
+      {
+        "title": "Ferrari F8",
+        "image": "https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg"
+      },
+      {
+        "title": "Lamborghini Huracan",
+        "image": "https://quatrorodas.abril.com.br/wp-content/uploads/2024/02/FiatMobiLikeMY245.jpg"
+      },
+    ],
+  };
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Carros'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Center(
-              child: Text('Carros aqui'),
-            ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Carros"),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: "Clássicos"),
+              Tab(text: "Luxo"),
+              Tab(text: "Esportivos"),
+            ],
           ),
-        ],
+        ),
+        body: TabBarView(
+          children: [
+            buildListView("Clássicos"),
+            buildListView("Luxo"),
+            buildListView("Esportivos"),
+          ],
+        ),
       ),
     );
   }
+
+  Widget buildListView(String category) {
+    final items = carData[category]!;
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        final item = items[index];
+        return Card(
+          margin: EdgeInsets.all(10),
+          child: ListTile(
+            leading: Image.network(
+              item["image"]!,
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+            title: Text(item["title"]!),
+          ),
+        );
+      },
+    );
+  }
+
 }
